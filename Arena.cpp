752f151794e39da23416eb02dddca2c52f970460
Agade09 <n.sillitoe@gmail.com>
2019-04-08 12:23:53 +0200

    Fixed indentation by converting tabs to spaces

--- Arena.cpp
+++ Arena.cpp
@@ -24,8 +24,8 @@ struct vec{
         return x!=a.x || y!=a.y;
     }
     inline void operator+=(const vec &a)noexcept{
-    	x+=a.x;
-    	y+=a.y;
+        x+=a.x;
+        y+=a.y;
     }
     inline double norm2()const noexcept{
         return pow(x,2)+pow(y,2);
@@ -34,13 +34,13 @@ struct vec{
         return sqrt(norm2());
     }
     inline vec operator+(const vec &a)const noexcept{
-    	return vec{x+a.x,y+a.y};
+        return vec{x+a.x,y+a.y};
     }
     inline vec operator-(const vec &a)const noexcept{
-    	return vec{x-a.x,y-a.y};
+        return vec{x-a.x,y-a.y};
     }
     inline vec operator*(const double &a)const noexcept{
-    	return vec{x*a,y*a};
+        return vec{x*a,y*a};
     }
 };
 
@@ -55,8 +55,8 @@ inline istream& operator>>(istream &is,vec &r)noexcept{
 }
 
 struct action{
-	vec t;
-	string thrust;
+    vec t;
+    string thrust;
 };
 
 typedef vector<vec> map;
@@ -77,10 +77,10 @@ struct pod{
 };
 
 struct AI{
-	int id,pid,outPipe,errPipe,inPipe;
-	string name;
-	pod p;
-	inline void stop(){
+    int id,pid,outPipe,errPipe,inPipe;
+    string name;
+    pod p;
+    inline void stop(){
         if(alive()){
             kill(pid,SIGTERM);
             int status;
@@ -90,20 +90,20 @@ struct AI{
             }
         }
     }
-	inline bool alive()const{
-		return kill(pid,0)!=-1;//Check if process is still running
-	}
-	inline void Feed_Inputs(const string &inputs){
+    inline bool alive()const{
+        return kill(pid,0)!=-1;//Check if process is still running
+    }
+    inline void Feed_Inputs(const string &inputs){
         if(write(inPipe,&inputs[0],inputs.size())!=inputs.size()){
             throw(5);
         }
     }
-	inline ~AI(){
-		close(errPipe);
-		close(outPipe);
-		close(inPipe);
-		stop();
-	}
+    inline ~AI(){
+        close(errPipe);
+        close(outPipe);
+        close(inPipe);
+        stop();
+    }
 };
 
 void StartProcess(AI &Bot){
@@ -176,8 +176,8 @@ inline double Angle_Sum(const double a,const double b)noexcept{
 }
 
 inline double Angle(const vec &d)noexcept{
-	double n{d.norm()};
-	double a{acos(d.x/n)*180/M_PI};
+    double n{d.norm()};
+    double a{acos(d.x/n)*180/M_PI};
     if(d.y<0){
         a=360-a;
     }
@@ -201,19 +201,19 @@ inline bool Passes_Checkpoint(const vec &r,const vec &v,const vec &t){
 }
 
 void Make_Move(const string &MoveStr,AI &Bot,const map &C,const int turn){
-	stringstream ss(MoveStr);
-	action Move;
-	ss >> Move.t >> Move.thrust;
-	pod &p=Bot.p;
-	if(Move.thrust=="SHIELD"){
-		cerr << "Found SHIELD" << endl;
-	}
-	const int thrust{Move.thrust=="BOOST"?650:Move.thrust=="SHIELD"?0:stoi(Move.thrust)};
-	const double desired_angle{Angle(Move.t-p.r)};
-	p.angle=turn==1?desired_angle:Closest_Angle(p.angle,desired_angle);
+    stringstream ss(MoveStr);
+    action Move;
+    ss >> Move.t >> Move.thrust;
+    pod &p=Bot.p;
+    if(Move.thrust=="SHIELD"){
+        cerr << "Found SHIELD" << endl;
+    }
+    const int thrust{Move.thrust=="BOOST"?650:Move.thrust=="SHIELD"?0:stoi(Move.thrust)};
+    const double desired_angle{Angle(Move.t-p.r)};
+    p.angle=turn==1?desired_angle:Closest_Angle(p.angle,desired_angle);
     vec new_v{p.v+vec{cos(p.angle*DegToRad),sin(p.angle*DegToRad)}*thrust};
     if(Passes_Checkpoint(p.r,new_v,C[p.next])){
-    	p.time=100;
+        p.time=100;
         ++p.next;
         if(p.next==C.size()){
             p.next=0;
@@ -244,17 +244,17 @@ inline string EmptyPipe(const int fd){
 }
 
 bool IsValidMove(const string &Move){
-	stringstream ss(Move);
-	for(int i=0;i<2;++i){
+    stringstream ss(Move);
+    for(int i=0;i<2;++i){
         string line;
         getline(ss,line);
         stringstream ss2(line);
-		action PodMove;
-		if(!(ss2 >> PodMove.t >> PodMove.thrust)){
-			return false;
-		}
-	}
-	return true;
+        action PodMove;
+        if(!(ss2 >> PodMove.t >> PodMove.thrust)){
+            return false;
+        }
+    }
+    return true;
 }
 
 string GetMove(AI &Bot,const int turn){
@@ -266,7 +266,7 @@ string GetMove(AI &Bot,const int turn){
         if(poll(&outpoll,1,TimeLeft)){
             out+=EmptyPipe(Bot.outPipe);
             if(IsValidMove(out)){
-            	return out;
+                return out;
             }
         }
     }
@@ -274,79 +274,79 @@ string GetMove(AI &Bot,const int turn){
 }
 
 inline map Generate_Map()noexcept{
-	default_random_engine generator(system_clock::now().time_since_epoch().count());
-	uniform_int_distribution<int> Map_Distrib(0,Maps.size()-1);
-	map C{Maps[Map_Distrib(generator)]};
-	uniform_int_distribution<int> Rotate_Distrib(0,C.size()-1),Delta_Distrib(-30,30);
-	rotate(C.begin(),C.begin()+Rotate_Distrib(generator),C.end());
-	for(vec &cp:C){
-		cp+=vec{static_cast<double>(Delta_Distrib(generator)),static_cast<double>(Delta_Distrib(generator))};
-	}
-	return C;
+    default_random_engine generator(system_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> Map_Distrib(0,Maps.size()-1);
+    map C{Maps[Map_Distrib(generator)]};
+    uniform_int_distribution<int> Rotate_Distrib(0,C.size()-1),Delta_Distrib(-30,30);
+    rotate(C.begin(),C.begin()+Rotate_Distrib(generator),C.end());
+    for(vec &cp:C){
+        cp+=vec{static_cast<double>(Delta_Distrib(generator)),static_cast<double>(Delta_Distrib(generator))};
+    }
+    return C;
 }
 
 int Play_Game(array <string,N> &Bot_Names){
-	map C=Generate_Map();
-	vector<AI> Bot(Bot_Names.size());
-	for(int i=0;i<Bot_Names.size();++i){
-		Bot[i].id=i;
-		Bot[i].name=Bot_Names[i];
-		StartProcess(Bot[i]);
-		Bot[i].p.r=C[0];
-		Bot[i].p.v=vec{0,0};
-		Bot[i].p.lap=0;
-		Bot[i].p.next=1;
-		Bot[i].p.angle=Angle(C[1]-C[0]);//Could do better?
-		Bot[i].p.time=100;
-	}
-	int turn{0};
-	//Feed first turn inputs
-	for(AI &b:Bot){
-		stringstream ss;
-		ss << N_L << endl << C.size() << endl;
-		for(const vec &cp:C){
-			ss << cp << endl;
-		}
-		b.Feed_Inputs(ss.str());
-	}
-	while(++turn>0 && !stop){
-		for(int i=0;i<Bot.size();++i){
-			if(Bot[i].alive()){
-				//Feed turn inputs
-				stringstream ss;
-				for(int j=0;j<2;++j){//Duplicate pod info
-					ss << Bot[i].p.r << " " << round(Bot[i].p.v.x) << " " << round(Bot[i].p.v.y) << " " << round(Bot[i].p.angle) << " " << Bot[i].p.next << endl;
-				}
-				for(int j=0;j<2;++j){//Feed fake far away opponent position
-					ss << -10000+1000*j << " " << -10000 << " " << 0 << " " << 0 << " " << 0 << " " << 1 << endl;
-				}
-				try{
-					Bot[i].Feed_Inputs(ss.str());
-					string Move{GetMove(Bot[i],turn)};
-					string err_str{EmptyPipe(Bot[i].errPipe)};
-					if(Debug_AI){
-						ofstream err_out("log.txt",ios::app);
-						err_out << err_str << endl;
-					}
-					Make_Move(Move,Bot[i],C,turn);
-					if(Bot[i].p.lap==N_L){
-						return turn;
-					}
-					else if(Bot[i].p.time==0){
-						return -2;
-					}
-				}
-				catch(...){
-					cerr << "Loss by timeout" << endl;
-					return -1;
-				}
-			}
-			else{
-				return -2;
-			}
-		}
-	}
-	return -2;
+    map C=Generate_Map();
+    vector<AI> Bot(Bot_Names.size());
+    for(int i=0;i<Bot_Names.size();++i){
+        Bot[i].id=i;
+        Bot[i].name=Bot_Names[i];
+        StartProcess(Bot[i]);
+        Bot[i].p.r=C[0];
+        Bot[i].p.v=vec{0,0};
+        Bot[i].p.lap=0;
+        Bot[i].p.next=1;
+        Bot[i].p.angle=Angle(C[1]-C[0]);//Could do better?
+        Bot[i].p.time=100;
+    }
+    int turn{0};
+    //Feed first turn inputs
+    for(AI &b:Bot){
+        stringstream ss;
+        ss << N_L << endl << C.size() << endl;
+        for(const vec &cp:C){
+            ss << cp << endl;
+        }
+        b.Feed_Inputs(ss.str());
+    }
+    while(++turn>0 && !stop){
+        for(int i=0;i<Bot.size();++i){
+            if(Bot[i].alive()){
+                //Feed turn inputs
+                stringstream ss;
+                for(int j=0;j<2;++j){//Duplicate pod info
+                    ss << Bot[i].p.r << " " << round(Bot[i].p.v.x) << " " << round(Bot[i].p.v.y) << " " << round(Bot[i].p.angle) << " " << Bot[i].p.next << endl;
+                }
+                for(int j=0;j<2;++j){//Feed fake far away opponent position
+                    ss << -10000+1000*j << " " << -10000 << " " << 0 << " " << 0 << " " << 0 << " " << 1 << endl;
+                }
+                try{
+                    Bot[i].Feed_Inputs(ss.str());
+                    string Move{GetMove(Bot[i],turn)};
+                    string err_str{EmptyPipe(Bot[i].errPipe)};
+                    if(Debug_AI){
+                        ofstream err_out("log.txt",ios::app);
+                        err_out << err_str << endl;
+                    }
+                    Make_Move(Move,Bot[i],C,turn);
+                    if(Bot[i].p.lap==N_L){
+                        return turn;
+                    }
+                    else if(Bot[i].p.time==0){
+                        return -2;
+                    }
+                }
+                catch(...){
+                    cerr << "Loss by timeout" << endl;
+                    return -1;
+                }
+            }
+            else{
+                return -2;
+            }
+        }
+    }
+    return -2;
 }
 
 void StopArena(const int signum){
@@ -354,44 +354,44 @@ void StopArena(const int signum){
 }
 
 int main(int argc,char **argv){
-	if(argc<2){
-		cerr << "Program takes 1 input, the names of the AI to test" << endl;
-		return 0;
-	}
-	int N_Threads{1};
-	if(argc>=3){//Optional N_Threads parameter
-		N_Threads=min(2*omp_get_num_procs(),max(1,atoi(argv[2])));
-		cerr << "Running " << N_Threads << " arena threads" << endl;
-	}
-	array<string,N> Bot_Names;
-	for(int i=0;i<N;++i){
-		Bot_Names[i]=argv[i+1];
-	}
-	cout << "Testing AI " << Bot_Names[0] << endl;
-	for(int i=0;i<Bot_Names.size();++i){
-		ifstream Test{Bot_Names[i].c_str()};
-		if(!Test){
-			cerr << Bot_Names[i] << " couldn't be found" << endl;
-			return 0;
-		}
-		Test.close();
-	}
-	signal(SIGTERM,StopArena);//Register SIGTERM signal handler so the arena can cleanup when you kill it
+    if(argc<2){
+        cerr << "Program takes 1 input, the names of the AI to test" << endl;
+        return 0;
+    }
+    int N_Threads{1};
+    if(argc>=3){//Optional N_Threads parameter
+        N_Threads=min(2*omp_get_num_procs(),max(1,atoi(argv[2])));
+        cerr << "Running " << N_Threads << " arena threads" << endl;
+    }
+    array<string,N> Bot_Names;
+    for(int i=0;i<N;++i){
+        Bot_Names[i]=argv[i+1];
+    }
+    cout << "Testing AI " << Bot_Names[0] << endl;
+    for(int i=0;i<Bot_Names.size();++i){
+        ifstream Test{Bot_Names[i].c_str()};
+        if(!Test){
+            cerr << Bot_Names[i] << " couldn't be found" << endl;
+            return 0;
+        }
+        Test.close();
+    }
+    signal(SIGTERM,StopArena);//Register SIGTERM signal handler so the arena can cleanup when you kill it
     signal(SIGPIPE,SIG_IGN);//Ignore SIGPIPE to avoid the arena crashing when an AI crashes
-	int maps_done{0},maps_failed{0};
-	double average_turns{0};
-	#pragma omp parallel num_threads(N_Threads) shared(average_turns,maps_done,Bot_Names)
-	while(!stop){
-		int turns{Play_Game(Bot_Names)};
-		if(turns>0){
-			#pragma omp atomic
-			average_turns+=(turns-average_turns)/(++maps_done);
-		}
-		else{
+    int maps_done{0},maps_failed{0};
+    double average_turns{0};
+    #pragma omp parallel num_threads(N_Threads) shared(average_turns,maps_done,Bot_Names)
+    while(!stop){
+        int turns{Play_Game(Bot_Names)};
+        if(turns>0){
+            #pragma omp atomic
+            average_turns+=(turns-average_turns)/(++maps_done);
+        }
+        else{
             ++maps_failed;
-			//cerr << "Bot didn't complete map" << endl;
-		}
-		#pragma omp critical
-		cerr << "Average turns: " << setprecision(5) << average_turns << " with " << maps_done << " maps done with " << setprecision(2) << 100.0*maps_failed/(maps_failed+maps_done) << "% maps failed" << endl;
-	}
+            //cerr << "Bot didn't complete map" << endl;
+        }
+        #pragma omp critical
+        cerr << "Average turns: " << setprecision(5) << average_turns << " with " << maps_done << " maps done with " << setprecision(2) << 100.0*maps_failed/(maps_failed+maps_done) << "% maps failed" << endl;
+    }
 }
